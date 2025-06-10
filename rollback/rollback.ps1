66449f0ea895b1abfb88ded48d60837221582c21
Philibert <146821558+gaelpidoux@users.noreply.github.com>
2025-06-10 16:51:59 +0200

    Update rollback.ps1

--- rollback/rollback.ps1
+++ rollback/rollback.ps1
@@ -1,7 +1,4 @@
 $timestamp = Get-Date -Format "yyyyMMdd_HHmmss"
-$backupPath = "C:\Backups\MyApp_$timestamp"
+$backupPath = "C:\Backups\MyApp_$timestamp"            #tourne pas encore donc je ne met pas de vrais chemin
 Copy-Item -Path "C:\Deploy\MyApp" -Destination $backupPath -Recurse
 Write-Host "Backup created at $backupPath"
-
-
---tourne pas encore donc je ne met pas de vrais chemin
