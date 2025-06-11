2571a3e745c2e427bdd900f2a0f884bb342642a8
Philibert <146821558+gaelpidoux@users.noreply.github.com>
2025-06-11 09:55:55 +0200

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "resource_group_name" {
+  default = "wpf-ci-rg"
+}
+
+variable "location" {
+  default = "West Europe"
+}
+
+variable "admin_username" {
+  default = "adminuser"
+}
+
+variable "admin_password" {
+  default = "P@ssword1234!"
+}
