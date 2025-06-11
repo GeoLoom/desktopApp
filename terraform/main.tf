7189afca52a06c6ebab9de0998576ba870c106b8
Philibert <146821558+gaelpidoux@users.noreply.github.com>
2025-06-11 09:53:59 +0200

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,23 @@
+resource "azurerm_windows_virtual_machine" "vm" {
+  name                  = "wpf-vm"
+  resource_group_name   = azurerm_resource_group.rg.name
+  location              = var.location
+  size                  = "Standard_B2s"
+  admin_username        = "adminuser"
+  admin_password        = "P@ssword1234!"
+
+  network_interface_ids = [azurerm_network_interface.nic.id]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+    name                 = "myosdisk"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2022-Datacenter"
+    version   = "latest"
+  }
+}
