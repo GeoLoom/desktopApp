42dba23beb89756c2fe02dd6e8b8fa2b69ce56c9
Philibert <146821558+gaelpidoux@users.noreply.github.com>
2025-06-11 09:55:27 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,17 +1,60 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "wpf-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "default"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_public_ip" "pip" {
+  name                = "wpf-vm-ip"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  allocation_method   = "Dynamic"
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "wpf-vm-nic"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip.id
+  }
+}
+
 resource "azurerm_windows_virtual_machine" "vm" {
-  name                  = "wpf-vm"
-  resource_group_name   = azurerm_resource_group.rg.name
-  location              = var.location
-  size                  = "Standard_B2s"
-  admin_username        = "adminuser"
-  admin_password        = "P@ssword1234!"
+  name                = "wpf-ci-vm"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  size                = "Standard_B2s"
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
 
   network_interface_ids = [azurerm_network_interface.nic.id]
 
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
-    name                 = "myosdisk"
+    name                 = "wpf-osdisk"
   }
 
   source_image_reference {
